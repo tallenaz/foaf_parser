8d2bcb092a306c41c9caaddb4aeccecc3ffbefa7
Anthony Zanella <azanella@stanford.edu>
2016-08-02 14:57:15 -0700

    instantiates with and returns a url

--- lib/foaf_parser.rb
+++ lib/foaf_parser.rb
@@ -1,4 +1,11 @@
 class FoafParser
+
+  attr_accessor :url
+
+  def initialize(rdf_url='http://stanford.edu/~azanella/foaf.rdf')
+    @url = rdf_url
+  end
+
   def speak
     "I'm FoafParser!"
   end
--- spec/lib/foaf_parser_spec.rb
+++ spec/lib/foaf_parser_spec.rb
@@ -11,4 +11,14 @@ describe FoafParser do
     fp = FoafParser.new()
     expect(fp.speak).to eq("I'm FoafParser!")
   end
+
+  it 'instantiates with a url' do
+    fp = FoafParser.new('http://stanford.edu/~azanella/foaf.rdf')
+    expect(fp).to be_kind_of(FoafParser)
+  end
+
+  it 'returns a url' do
+    fp = FoafParser.new('http://stanford.edu/~azanella/foaf.rdf')
+    expect(fp.url).to eq('http://stanford.edu/~azanella/foaf.rdf')
+  end
 end
